20adeae897331cc5fef34f9aa0c93f8d25ac0a7b
Takumi Nogami <s16c1097nc@s.chibakoudai.jp>
2019-01-14 20:55:56 +0900

    deleted files

--- ros_start/scripts/listener.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from std_msgs.msg import String
-def callback(message):
-    rospy.loginfo("I heard %s", message.data)
-    print "ok."
-rospy.init_node('listener')
-sub = rospy.Subscriber('chatter', String, callback)
-rospy.spin()
